57116584ebd2a4da8754091f217dddc1bf326743
Franco Maurizzio <73903997+FrannM@users.noreply.github.com>
2021-08-29 20:25:47 -0300

    Add project files.

---
+++ Entidades/Autor.cs
@@ -0,0 +1,24 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebApiAutores.Validaciones;
+
+namespace WebApiAutores.Entidades;
+public class Autor
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "El campo {0} es requerido")]
+    [StringLength(maximumLength: 20, ErrorMessage = "El campo {0} no debe de tener más de {1} carácteres")]
+    [PrimeraLetraMayuscula]
+    public string Nombre { get; set; }
+    [Range(18, 80)]
+    [NotMapped]
+    public int Edad { get; set; }
+    [CreditCard]
+    [NotMapped]
+    public string TarjetaDeCredito { get; set; }
+    [Url]
+    [NotMapped]
+    public string URL { get; set; }
+    public List<Libro> Libros { get; set; }
+}
